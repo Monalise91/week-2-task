de885b197026ec224a6cbfdc9b4670965b04c7b9
Monalise91 <157795079+Monalise91@users.noreply.github.com>
2024-02-03 22:49:05 +0100

    week 2 task
    i did not understand this task at the beginning. now I am getting it now.

---
+++ week 2 task.py
@@ -0,0 +1,12 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 2
+>>> y = 13
+>>> y - x
+11
+>>> x + y
+15
+>>> x * y
+26
+>>> y / x
+6.5
